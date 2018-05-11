e07dec4745c11f4d6581fd4094b66896be741e68
银城鬼龙 <497163137@qq.com>
2018-05-11 14:53:48 +0800

    spring boot集成shiro+cas失败，注释相关代码

--- src/main/java/com/tmhp/platform/core/config/ShiroCasConfiguration.java
+++ src/main/java/com/tmhp/platform/core/config/ShiroCasConfiguration.java
@@ -1,123 +1,117 @@
-package com.tmhp.platform.core.config;
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import javax.servlet.Filter;
-
-import org.apache.shiro.cache.MemoryConstrainedCacheManager;
-import org.apache.shiro.cas.CasFilter;
-import org.apache.shiro.cas.CasSubjectFactory;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.filter.authc.LogoutFilter;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.DelegatingFilterProxy;
-
-import com.tmhp.platform.core.security.MyShiroCasRealm;
-
-/***
- * *
- * @author zqf
- * @date 2018年5月4日
- */
-@Configuration
-public class ShiroCasConfiguration {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ShiroCasConfiguration1.class);
-
-    private static final String CAS_FILTER_URL_PATTERN = "/shiro-cas";
-
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
-        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
-        filterRegistration.addInitParameter("targetFilterLifecycle", "true");
-        filterRegistration.setEnabled(true);
-        filterRegistration.addUrlPatterns("/*");
-        return filterRegistration;
-    }
-
-    @Bean(name = "lifecycleBeanPostProcessor")
-    public LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
-        return new LifecycleBeanPostProcessor();
-    }
-
-    @Bean(name = "securityManager")
-    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Value("${shiro.cas}") String casServerUrlPrefix,
-            @Value("${shiro.server}") String shiroServerUrlPrefix) {
-        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        //        CasRealm casRealm = new CasRealm();
-        //        casRealm.setDefaultRoles("ROLE_USER");
-        //        casRealm.setCasServerUrlPrefix(casServerUrlPrefix);
-        //        casRealm.setCasService(shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN);
-        //        securityManager.setRealm(casRealm);
-        securityManager.setRealm(this.getMyShiroCasRealm(casServerUrlPrefix, shiroServerUrlPrefix));
-        securityManager.setCacheManager(new MemoryConstrainedCacheManager());
-        securityManager.setSubjectFactory(new CasSubjectFactory());
-        return securityManager;
-    }
-
-    private MyShiroCasRealm getMyShiroCasRealm(String casServerUrlPrefix, String shiroServerUrlPrefix) {
-        MyShiroCasRealm casRealm = new MyShiroCasRealm();
-        casRealm.setCasServerUrlPrefix(casServerUrlPrefix);
-        casRealm.setCasService(shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN);
-        return casRealm;
-    }
-
-    private void loadShiroFilterChain(ShiroFilterFactoryBean shiroFilterFactoryBean) {
-        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-        LOGGER.info("###### 从数据库读取权限规则，加载到shiroFilter中 ######");
-        // TODO 从数据库读取权限规则
-        filterChainDefinitionMap.put(CAS_FILTER_URL_PATTERN, "casFilter");
-        filterChainDefinitionMap.put("/login", "casFilter");
-        filterChainDefinitionMap.put("/logout", "logout");
-        filterChainDefinitionMap.put("/**", "authc");
-        // 不需要拦截的静态文件请求
-        filterChainDefinitionMap.put("/static", "anon");
-        // 不过滤其他业务系统请求
-        filterChainDefinitionMap.put("/pyhz/*", "anon");
-        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-    }
-
-    /**
-     * CAS Filter
-     */
-    @Bean(name = "casFilter")
-    public CasFilter getCasFilter(@Value("${shiro.cas}") String casServerUrlPrefix, @Value("${shiro.server}") String shiroServerUrlPrefix) {
-        CasFilter casFilter = new CasFilter();
-        casFilter.setName("casFilter");
-        casFilter.setEnabled(true);
-        String loginUrl = casServerUrlPrefix + "/login?service=" + shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN;
-        casFilter.setFailureUrl(loginUrl);
-        return casFilter;
-    }
-
-    @Bean(name = "shiroFilter")
-    public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager securityManager, CasFilter casFilter,
-            @Value("${shiro.cas}") String casServerUrlPrefix, @Value("${shiro.server}") String shiroServerUrlPrefix) {
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        shiroFilterFactoryBean.setSecurityManager(securityManager);
-        String loginUrl = casServerUrlPrefix + "/login?service=" + shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN;
-        shiroFilterFactoryBean.setLoginUrl(loginUrl);
-        shiroFilterFactoryBean.setSuccessUrl("/");
-        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
-        Map<String, Filter> filters = new HashMap<>();
-        filters.put("casFilter", casFilter);
-        LogoutFilter logoutFilter = new LogoutFilter();
-        logoutFilter.setRedirectUrl(casServerUrlPrefix + "/logout?service=" + shiroServerUrlPrefix);
-        filters.put("logout", logoutFilter);
-        shiroFilterFactoryBean.setFilters(filters);
-
-        this.loadShiroFilterChain(shiroFilterFactoryBean);
-        return shiroFilterFactoryBean;
-    }
-}
+//package com.tmhp.platform.core.config;
+//
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+//import javax.servlet.Filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.filter.DelegatingFilterProxy;
+//
+///***
+// * *
+// * @author zqf
+// * @date 2018年5月4日
+// */
+//@Configuration
+//public class ShiroCasConfiguration {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(ShiroCasConfiguration.class);
+//
+//    private static final String CAS_FILTER_URL_PATTERN = "/shiro-cas";
+//
+//    @SuppressWarnings({ "rawtypes", "unchecked" })
+//    @Bean
+//    public FilterRegistrationBean filterRegistrationBean() {
+//        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+//        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
+//        filterRegistration.addInitParameter("targetFilterLifecycle", "true");
+//        filterRegistration.setEnabled(true);
+//        filterRegistration.addUrlPatterns("/*");
+//        return filterRegistration;
+//    }
+//
+//    @Bean(name = "lifecycleBeanPostProcessor")
+//    public LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
+//        return new LifecycleBeanPostProcessor();
+//    }
+//
+//    @Bean(name = "securityManager")
+//    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Value("${shiro.cas}") String casServerUrlPrefix,
+//            @Value("${shiro.server}") String shiroServerUrlPrefix) {
+//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+//        //        CasRealm casRealm = new CasRealm();
+//        //        casRealm.setDefaultRoles("ROLE_USER");
+//        //        casRealm.setCasServerUrlPrefix(casServerUrlPrefix);
+//        //        casRealm.setCasService(shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN);
+//        //        securityManager.setRealm(casRealm);
+//        securityManager.setRealm(this.getMyShiroCasRealm(casServerUrlPrefix, shiroServerUrlPrefix));
+//        securityManager.setCacheManager(new MemoryConstrainedCacheManager());
+//        securityManager.setSubjectFactory(new CasSubjectFactory());
+//        return securityManager;
+//    }
+//
+//    private MyShiroCasRealm getMyShiroCasRealm(String casServerUrlPrefix, String shiroServerUrlPrefix) {
+//        MyShiroCasRealm casRealm = new MyShiroCasRealm();
+//        casRealm.setCasServerUrlPrefix(casServerUrlPrefix);
+//        casRealm.setCasService(shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN);
+//        return casRealm;
+//    }
+//
+//    private void loadShiroFilterChain(ShiroFilterFactoryBean shiroFilterFactoryBean) {
+//        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+//        LOGGER.info("###### 从数据库读取权限规则，加载到shiroFilter中 ######");
+//        // TODO 从数据库读取权限规则
+//        filterChainDefinitionMap.put(CAS_FILTER_URL_PATTERN, "casFilter");
+//        filterChainDefinitionMap.put("/login", "authc");
+//        filterChainDefinitionMap.put("/logout", "logout");
+//        filterChainDefinitionMap.put("/user/edit/**", "authc,perms[user:edit]");
+//        filterChainDefinitionMap.put("/**", "authc");
+//        // 不需要拦截的静态文件请求
+//        filterChainDefinitionMap.put("/static", "anon");
+//        // 不过滤其他业务系统请求
+//        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+//    }
+//
+//    /**
+//     * CAS Filter
+//     */
+//    @Bean(name = "casFilter")
+//    public CasFilter getCasFilter(@Value("${shiro.cas}") String casServerUrlPrefix, @Value("${shiro.server}") String shiroServerUrlPrefix) {
+//        CasFilter casFilter = new CasFilter();
+//        casFilter.setName("casFilter");
+//        casFilter.setEnabled(true);
+//        String loginUrl = casServerUrlPrefix + "/login?service=" + shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN;
+//        //String successUrl = shiroServerUrlPrefix + "/user";
+//        casFilter.setFailureUrl(loginUrl);
+//        //casFilter.setSuccessUrl(successUrl);
+//        return casFilter;
+//    }
+//
+//    @Bean(name = "shiroFilter")
+//    public ShiroFilterFactoryBean getShiroFilterFactoryBean(DefaultWebSecurityManager securityManager, CasFilter casFilter,
+//            @Value("${shiro.cas}") String casServerUrlPrefix, @Value("${shiro.server}") String shiroServerUrlPrefix) {
+//        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+//        shiroFilterFactoryBean.setSecurityManager(securityManager);
+//        String loginUrl = casServerUrlPrefix + "/login?service=" + shiroServerUrlPrefix + CAS_FILTER_URL_PATTERN;
+//        //shiroFilterFactoryBean.setLoginUrl(casServerUrlPrefix + "/login?service=" + shiroServerUrlPrefix + "/user");
+//        shiroFilterFactoryBean.setLoginUrl(loginUrl);
+//        shiroFilterFactoryBean.setSuccessUrl("/user");
+//        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+//        Map<String, Filter> filters = new HashMap<>();
+//        filters.put("casFilter", casFilter);
+//        LogoutFilter logoutFilter = new LogoutFilter();
+//        logoutFilter.setRedirectUrl(casServerUrlPrefix + "/logout?service=" + shiroServerUrlPrefix);
+//        filters.put("logout", logoutFilter);
+//        shiroFilterFactoryBean.setFilters(filters);
+//
+//        this.loadShiroFilterChain(shiroFilterFactoryBean);
+//        return shiroFilterFactoryBean;
+//    }
+//}
--- src/main/java/com/tmhp/platform/core/security/MyShiroCasRealm.java
+++ src/main/java/com/tmhp/platform/core/security/MyShiroCasRealm.java
@@ -1,82 +1,80 @@
-package com.tmhp.platform.core.security;
-
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.cas.CasRealm;
-import org.apache.shiro.subject.PrincipalCollection;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.tmhp.platform.module.user.domain.User;
-import com.tmhp.platform.module.user.mapper.UserMapper;
-
-/***
- * @author zqf
- * @date 2018年5月3日
- * 
- */
-public class MyShiroCasRealm extends CasRealm {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MyShiroCasRealm.class);
-
-    @Autowired
-    private UserMapper userDao;
-
-    /**
-     * 单Cas服务登录校验通过后，便会调用这个方法，并携带用户信息的Token参数
-     * 假设只要是有Token过来，就说明是有效的登录用户，不再对密码等做校验
-     * 方法名称 : doGetAuthenticationInfo
-     * 功能描述 : 验证当前登陆的Subject
-     * @param authcToken 当前登录用户的token
-     * @return 验证信息
-     */
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) {
-        // UsernamePasswordToken对象用来存放提交的登录信息
-        UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
-        //        AuthenticationInfo token = super.doGetAuthenticationInfo(authcToken);
-        //        String username = (String) token.getPrincipals().getPrimaryPrincipal();
-        LOGGER.info("当前Subject时获取到用户名为" + token.getUsername());
-        User user = this.userDao.getByName(token.getUsername());
-        if (user != null) {
-            // 若存在，将此用户存放到登录认证info中，无需自己做密码对比，Shiro会为我们进行密码对比校验
-            return new SimpleAuthenticationInfo(user.getUsername(), user.getPassword(), this.getName());
-            //            SecurityUtils.getSubject().getSession().setAttribute("user", user);
-        }
-        return null;
-    }
-
-    /**
-     * 权限认证，为当前登录的Subject授予角色和权限
-     * @see 经测试：本例中该方法的调用时机为需授权资源被访问时
-     * @see 经测试：并且每次访问需授权资源时都会执行该方法中的逻辑，这表明本例中默认并未启用AuthorizationCache
-     * @see 经测试：如果连续访问同一个URL（比如刷新），该方法不会被重复调用，Shiro有一个时间间隔（也就是cache时间，在ehcache-shiro.xml中配置），超过这个时间间隔再刷新页面，该方法会被执行
-     */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        LOGGER.info("################## 执行Shiro权限认证 ##################");
-        // 获取当前登录输入的用户名，等价于(String) principalCollection.fromRealm(getName()).iterator().next();
-        String userName = (String) super.getAvailablePrincipal(principalCollection);
-        // 到数据库查是否有此对象
-        // 实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
-        User user = this.userDao.getByName(userName);
-        if (user != null) {
-            // 权限信息对象info，用来存放查出的用户的所有的角色（role）及权限（permission）
-            SimpleAuthorizationInfo auth = new SimpleAuthorizationInfo();
-            // 用户的角色集合
-            auth.setRoles(user.getRolesName());
-            // 用户的角色对应的所有权限，如果只使用角色定义访问权限，下面的四行可以不要
-            user.getRoleList().forEach(role -> {
-                auth.addStringPermissions(role.getPermissionNames());
-            });
-            return auth;
-        }
-        // 返回null的话，就会导致任何用户访问被拦截的请求时，都会自动跳转到unauthorizedUrl指定的地址
-        return null;
-    }
-}
+//package com.tmhp.platform.core.security;
+//
+//import org.apache.shiro.SecurityUtils;
+//import org.apache.shiro.authc.AuthenticationException;
+//import org.apache.shiro.authc.AuthenticationInfo;
+//import org.apache.shiro.authc.AuthenticationToken;
+//import org.apache.shiro.authz.AuthorizationInfo;
+//import org.apache.shiro.authz.SimpleAuthorizationInfo;
+//import org.apache.shiro.cas.CasRealm;
+//import org.apache.shiro.subject.PrincipalCollection;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.tmhp.platform.module.user.domain.User;
+//import com.tmhp.platform.module.user.mapper.UserMapper;
+//
+///***
+// * @author zqf
+// * @date 2018年5月3日
+// *
+// */
+//public class MyShiroCasRealm extends CasRealm {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(MyShiroCasRealm.class);
+//
+//    @Autowired
+//    private UserMapper userDao;
+//
+//    /**
+//     * 登录验证
+//     * @param authcToken 当前登录用户的token
+//     * @return AuthenticationInfo
+//     */
+//    @Override
+//    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+//        // UsernamePasswordToken对象用来存放提交的登录信息
+//        //UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
+//        LOGGER.info("开始进行登录验证");
+//        AuthenticationInfo token = super.doGetAuthenticationInfo(authcToken);
+//        String username = (String) token.getPrincipals().getPrimaryPrincipal();
+//        LOGGER.info("当前Subject时获取到用户名为" + username);
+//        User user = this.userDao.getByName(username);
+//        if (user != null) {
+//            // 若存在，将此用户存放到登录认证info中，无需自己做密码对比，Shiro会为我们进行密码对比校验
+//            //return new SimpleAuthenticationInfo(user.getUsername(), user.getPassword(), this.getName());
+//            SecurityUtils.getSubject().getSession().setAttribute("user", user);
+//        }
+//        LOGGER.error("登录认证失败！！！" + username);
+//        return null;
+//    }
+//
+//    /**
+//     * 权限认证
+//     * @param principalCollection
+//     * @return AuthorizationInfo
+//     */
+//    @Override
+//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+//        LOGGER.info("################## 执行Shiro权限认证 ##################");
+//        // 获取当前登录输入的用户名，等价于(String) principalCollection.fromRealm(getName()).iterator().next();
+//        String userName = (String) super.getAvailablePrincipal(principalCollection);
+//        // 到数据库查是否有此对象
+//        // 实际项目中，这里可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
+//        User user = this.userDao.getByName(userName);
+//        if (user != null) {
+//            // 权限信息对象info，用来存放查出的用户的所有的角色（role）及权限（permission）
+//            SimpleAuthorizationInfo auth = new SimpleAuthorizationInfo();
+//            // 用户的角色集合
+//            auth.setRoles(user.getRolesName());
+//            // 用户的角色对应的所有权限，如果只使用角色定义访问权限，下面的四行可以不要
+//            user.getRoleList().forEach(role -> {
+//                auth.addStringPermissions(role.getPermissionNames());
+//            });
+//            return auth;
+//        }
+//        // 返回null的话，就会导致任何用户访问被拦截的请求时，都会自动跳转到unauthorizedUrl指定的地址
+//        return null;
+//    }
+//}
